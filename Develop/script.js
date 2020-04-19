c00db233b07fbcb2ab46106a182317f76dc3f5ef
wazshaiwayana <61263457+wazshaiwayana@users.noreply.github.com>
2020-04-18 18:54:30 -0700

    Add files via upload

---
+++ Develop/script.js
@@ -0,0 +1,36 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+var howMany= prompt("how many characters do you want to choose?");
+
+var specialChar= confirm("do you want special characters?");
+var uppers = confirm("Would you like to use uppercase letters?");
+var lowers = confirm("Would you like to use lowercase letters?");
+var numbers = confirm("Would you like to use numbers?");
+var symbols = confirm("Would you like to use special characters?")
+
+function generatePassword() {
+  var length = howMany,
+      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*",
+      retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+      retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+
+}
+
+
